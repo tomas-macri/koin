0e31d42a00b1de125d7f2f0744e4a1f28064d71a
Tomás Macri <tomas.macri@vasscompany.com>
2023-11-24 10:12:36 +0100

    Upgrade dependencies and issue status in KoinAnnotation project

--- Koin Annotations/app/src/main/java/com/example/koinannotations/utils/StringProvider.kt
+++ Koin Annotations/app/src/main/java/com/example/koinannotations/utils/StringProvider.kt
@@ -1,17 +1,17 @@
 package com.example.koinannotations.utils
 
-import android.content.Context
 import androidx.annotation.StringRes
 import com.example.koinannotations.KoinAnnotationsApp
 import org.koin.core.annotation.Single
 
 @Single
-class StringProvider {
+class StringProvider() {
 
     fun getString(@StringRes stringResId: Int): String {
         //TODO: BUG en la release 1.3.0 de Koin Annotations - Al usar la CONFIG CHECK (arg("KOIN_CONFIG_CHECK","true"))
         // no encuentra la variable de tipo Context a pesar de inicializarla en el starKoin { }
         // See https://github.com/InsertKoinIO/koin-annotations/issues/94
+        //     https://github.com/InsertKoinIO/koin-annotations/issues/96
         return KoinAnnotationsApp().applicationContext.getString(stringResId)
     }
 }
